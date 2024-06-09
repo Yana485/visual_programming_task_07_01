403c663f1ee09e868882efa15b92b75872dd3599
Yana485 <105270905+Yana485@users.noreply.github.com>
2024-06-09 11:38:00 +0100

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace task7._01
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+
+namespace task7._01
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void customControl_Click(object sender, RoutedEventArgs e)
+        {
+            txtBlock.Text = "Ви щойно натиснули на кнопку";
+        }
+    }
+}
